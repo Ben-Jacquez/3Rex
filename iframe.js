9f49aa1dcd607cc3a88923616cc2eb807fd146be
Ben Jacquez <ben@Bens-MBP.attlocal.net>
2022-12-10 15:57:23 -0800

    a revert point incase i break it all

--- iframe.js
+++ iframe.js
@@ -1,6 +1,12 @@
-function() {
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '201fb6c1e4mshb63d3ed158cd9c9p14f35fjsnb3474b2899d8',
+		'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
+	}
+};
 
-}
-
-// <input onkeyup="getanswer(document.getElementById('qurybox').value)" id="qurybox"><div id="answer"></div>
-// <script src="https://"
+fetch('https://online-movie-database.p.rapidapi.com/auto-complete?q=bullet%20train', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err)):
---
+++ imdb.js
@@ -0,0 +1,25 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '201fb6c1e4mshb63d3ed158cd9c9p14f35fjsnb3474b2899d8',
+		'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
+	}
+};
+
+fetch('https://online-movie-database.p.rapidapi.com/auto-complete?q=bullet%20train', options)
+
+    .then(response => response.json())
+    .then(data => {
+        const list = data.d;
+
+        list.map((item) => {
+            const name = item.l;
+            const poster = item.i.imageurl;
+            const movie = `<li><img src="${poster}"> <h2>${name}</h2></li>`
+            document.querySelector('.movies').innerHTML += movie;
+
+        })
+    })
+    .catch(err => {
+        console.error(err);
+    });
