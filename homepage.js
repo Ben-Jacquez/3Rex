7e3d31a6d9e4706d0914fa05fc38f8e4f14e8936
saramangelo <saramangelo@gmail.com>
2022-12-11 21:25:02 -0800

    added logic to save clicks on dropdown buttons on index.html - successful besides the key name

--- homepage.js
+++ homepage.js
@@ -6,15 +6,55 @@ $(document).ready(function(){
   });
 
 
-  var comedyClick = $(".comedy");
-  var actionClick = $(".action");
+  var comedyClick = document.querySelector(".comedy");
+  var actionClick = document.querySelector(".action");
+  var netflixClick =  document.querySelector(".netflix");
+  var disneyClick = document.querySelector(".Disney");
 
 
-  function saveMovieChoice(){
-    console.log("clicked")
+  function saveComedyChoice(event){
+    // console.log("clicked comedy");
+    var comedy = event.target;
+    // console.log(event)
+    var key = comedy;
+    var value = true;
+    localStorage.setItem(key, value);
+
   }
 
-  comedyClick.onclick = function(){
-    
+  function saveActionChoice(event){
+    // console.log("clicked action");
+    var action = event.target;
+    // console.log(event)
+    var key = action;
+    var value = true;
+    localStorage.setItem(key, value);
   }
+
+  function saveNetflixChoice(event){
+    // console.log("clicked netflix");
+    var netflix = event.target;
+    // console.log(event)
+    var key = netflix;
+    var value = true;
+    localStorage.setItem(key, value);
+  }
+
+  function saveDisneyChoice(event){
+    // console.log("clicked disney");
+    var disney = event.target;
+    // console.log(event)
+    var key = disney;
+    var value = true;
+    localStorage.setItem(key, value);
+  }
+
+
+
+
+  comedyClick.addEventListener("click", saveComedyChoice);
+  actionClick.addEventListener("click", saveActionChoice);
+  netflixClick.addEventListener("click", saveNetflixChoice);
+  disneyClick.addEventListener("click", saveDisneyChoice);
+
  
