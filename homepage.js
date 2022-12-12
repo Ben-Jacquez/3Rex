fabf3a7e7f0ef67931acdb216bcebcf0159980db
saramangelo <saramangelo@gmail.com>
2022-12-11 20:42:02 -0800

    working on onclick for dropdown

--- homepage.js
+++ homepage.js
@@ -2,6 +2,19 @@ $('.dropdown-toggle').dropdown();
 
 $(document).ready(function(){
     $(".dropdown-toggle").dropdown();
+    console.log($('.dropdown-toggle'))
   });
 
-  
+
+  var comedyClick = $(".comedy");
+  var actionClick = $(".action");
+
+
+  function saveMovieChoice(){
+    console.log("clicked")
+  }
+
+  comedyClick.onclick = function(){
+    
+  }
+ 
